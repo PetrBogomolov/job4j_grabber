9f8b8309ee183362598c90b553f1c883b843d7a1
PetrBogomolov <BogomoloffPD@yandex.ru>
2021-01-26 11:16:09 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#408017]

--- src/main/java/jsoup/SqlRuParse.java
+++ src/main/java/jsoup/SqlRuParse.java
@@ -6,8 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.*;
 
 public class SqlRuParse {
     public static String formatDate(String date) {
@@ -28,14 +27,38 @@ public class SqlRuParse {
         return date;
     }
 
-    public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
+    public static List<String> selectPage(Document doc, String cssQuerry, int numberPage) throws IOException {
+        List<String> result = new ArrayList<>();
+        Elements pages = doc.select(cssQuerry);
+        for (Element el : pages.select("a")) {
+            while (Integer.parseInt(el.text()) <= numberPage) {
+                result.add(el.attr("href"));
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void showVacancies(Document doc, String cssQuery) {
+        Elements row = doc.select(cssQuery);
         for (Element td : row) {
             Element href = td.child(0);
             Element data = td.parent().child(5);
             System.out.println(href.text());
             System.out.printf("%s %s\n", href.attr("href"), formatDate(data.text()));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String url = "https://www.sql.ru/forum/job-offers";
+        String filterPage = ".sort_options";
+        String vacancy = ".postslisttopic";
+        Document pageOfWebsite = Jsoup.connect(url).get();
+        showVacancies(pageOfWebsite, vacancy);
+        for (String page : selectPage(pageOfWebsite, filterPage, 5)) {
+            url = page;
+            pageOfWebsite = Jsoup.connect(url).get();
+            showVacancies(pageOfWebsite, ".postslisttopic");
         }
     }
 }

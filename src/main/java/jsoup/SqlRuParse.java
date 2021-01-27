1108cd4da14848a1ae9e453b6bdb47bb52884873
PetrBogomolov <BogomoloffPD@yandex.ru>
2021-01-27 14:48:12 +0300

    2.1. Преобразование даты [#408021]

--- src/main/java/jsoup/SqlRuParse.java
+++ src/main/java/jsoup/SqlRuParse.java
@@ -5,29 +5,81 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+import static java.util.Map.entry;
+
 public class SqlRuParse {
-    public static String formatDate(String date) {
-        SimpleDateFormat patternOfData = new SimpleDateFormat("d MMM yy");
-        if (date.contains("сегодня")) {
-            String[] elementOfDate = date.split(",");
-            Calendar calendar = new GregorianCalendar();
-            elementOfDate[0] = patternOfData.format(calendar.getTime()).replace(".", "");
-            date = String.format("%s,%s", elementOfDate[0], elementOfDate[1]);
+    private final static String YESTERDAY = "вчера";
+    private final static String TODAY = "сегодня";
+    private static final SimpleDateFormat patternOfData = new SimpleDateFormat("d MMM yy");
+    private static final SimpleDateFormat patternOfDataForConvert =
+            new SimpleDateFormat("d MMM yy, HH:mm", Locale.ENGLISH);
+    private static final Map<String, String> months = Map.ofEntries(
+                    entry("янв", "jun"),
+                    entry("фев", "feb"),
+                    entry("мар", "mar"),
+                    entry("апр", "apr"),
+                    entry("май", "may"),
+                    entry("июн", "jun"),
+                    entry("июл", "jul"),
+                    entry("авг", "aug"),
+                    entry("сен", "sep"),
+                    entry("окт", "oct"),
+                    entry("ноя", "nov"),
+                    entry("дек", "dec")
+    );
+
+    /*
+        Метод преобразует String формат даты к Date.
+        Для этого функция меняет названия месяцев с русских на английские, затем производит смену формата
+     */
+    public static Date convertStringToDate(String date) throws ParseException {
+        Date result = new Date();
+        for (String month : months.keySet()) {
+            if (date.contains(month)) {
+                date = date.replace(month, months.get(month));
+                result = patternOfDataForConvert.parse(date);
+            }
         }
-        if (date.contains("вчера")) {
-            String[] elementOfDate = date.split(",");
-            Calendar calendar = Calendar.getInstance();
-            calendar.add(Calendar. DAY_OF_MONTH, -1);
-            elementOfDate[0] = patternOfData.format(calendar.getTime()).replace(".", "");
-            date = String.format("%s,%s", elementOfDate[0], elementOfDate[1]);
+        return result;
+    }
+
+    /*
+        Метод приводит записи всех дат, полученных с сайта, к одному формату "d MMM yy, HH:mm"
+     */
+    public static String setSingleStringDateStandard(String date) {
+        if (date.contains(TODAY)) {
+            return formatTodayDate(date);
+        }
+        if (date.contains(YESTERDAY)) {
+          return formatYesterdayDate(date);
         }
         return date;
     }
+    /*
+        Метод меняет запись сегодняшней даты с прописного формата на числовой
+     */
+    private static String formatTodayDate(String today) {
+        String[] elementOfDate = today.split(",");
+        Calendar calendar = new GregorianCalendar();
+        elementOfDate[0] = patternOfData.format(calendar.getTime()).replace(".", "");
+        return String.format("%s,%s", elementOfDate[0], elementOfDate[1]);
+    }
+    /*
+        Метод меняет запись вчерашней даты с прописного формата на числовой
+     */
+    private static String formatYesterdayDate(String yesterday) {
+        String[] elementOfDate = yesterday.split(",");
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar. DAY_OF_MONTH, -1);
+        elementOfDate[0] = patternOfData.format(calendar.getTime()).replace(".", "");
+        return String.format("%s,%s", elementOfDate[0], elementOfDate[1]);
+    }
 
-    public static void showVacancies(Document doc, String cssQuery) {
+    public static void showVacancies(Document doc, String cssQuery) throws ParseException {
         Elements row = doc.select(cssQuery);
         for (Element td : row) {
             Element href = td.child(0);
@@ -37,7 +89,7 @@ public class SqlRuParse {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         for (int numberOfPage = 1; numberOfPage <= 5; numberOfPage++) {
             String url = String.format("https://www.sql.ru/forum/job-offers/%d", numberOfPage);
             Document website = Jsoup.connect(url).get();

67e14dff398843976bc3349e8fb45308c1d44a2d
PetrBogomolov <BogomoloffPD@yandex.ru>
2021-01-29 10:24:18 +0300

    4. Схема таблицы Post [#408024]

--- src/main/java/db/create_table_post.sql
+++ src/main/java/db/create_table_post.sql
@@ -1,8 +1,8 @@
 CREATE TABLE post
 (
     id      serial primary key,
-    name    varchar(255) UNIQUE,
+    name    varchar(255),
     text    text,
-    link    varchar(255),
+    link    varchar(255) UNIQUE,
     created timestamp(0)
 );
